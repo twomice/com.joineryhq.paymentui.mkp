3dbc4ccac4354af46846cad7626467b2624d82f9
Anum Goel <agoel@backofficethinking.com>
2016-07-29 15:40:01 -0400

    Payment user interface extension

---
+++ CRM/Paymentui/BAO/Paymentui.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ *
+ * @package BOT Roundlake Payment User Interface Helper functions
+ * $Id$
+ *
+ */
+class CRM_Paymentui_BAO_Paymentui extends CRM_Event_DAO_Participant {
+  static function getParticipantInfo( $contactID ) {
+	$relatedContactIDs   = self::getRelatedContacts( $contactID );
+	$relatedContactIDs[] = $contactID;
+	$relContactIDs       = implode ( ',', $relatedContactIDs );
+	
+	//Get participant info for the primary and related contacts
+	$sql = "SELECT p.id, p.contact_id, e.title, c.display_name, pp.contribution_id FROM civicrm_participant p
+			INNER JOIN civicrm_contact c ON ( p.contact_id =  c.id )
+			INNER JOIN civicrm_event e ON ( p.event_id = e.id )
+			INNER JOIN civicrm_participant_payment pp ON ( p.id = pp.participant_id )
+			WHERE
+			p.contact_id IN ($relContactIDs)
+			AND (p.status_id = 14 OR p.status_id = 5) AND p.is_test = 0";
+	$dao = CRM_Core_DAO::executeQuery($sql);
+	
+	if ( $dao->N ) {		
+		while( $dao->fetch() ) {
+			//Get the payment details of all the participants
+			$paymentDetails = CRM_Contribute_BAO_Contribution::getPaymentInfo( $dao->id, 'event', FALSE, TRUE);	
+			//Get display names of the participants and additional participants, if any
+			$displayNames   = self::getDisplayNames( $dao->id, $dao->display_name);		
+			
+			//Create an array with all the participant and payment information
+			$participantInfo[$dao->id]['pid']             = $dao->id;
+			$participantInfo[$dao->id]['cid']             = $dao->contact_id;
+			$participantInfo[$dao->id]['contribution_id'] = $dao->contribution_id;
+			$participantInfo[$dao->id]['event_name']      = $dao->title;
+			$participantInfo[$dao->id]['contact_name']    = $displayNames;
+			$participantInfo[$dao->id]['total_amount']    = $paymentDetails['total'];
+			$participantInfo[$dao->id]['paid']            = $paymentDetails['paid'];
+			$participantInfo[$dao->id]['balance']         = $paymentDetails['balance'];
+			$participantInfo[$dao->id]['rowClass']        = 'row_'.$dao->id;			
+			$participantInfo[$dao->id]['payLater']        = $paymentDetails['payLater'];	
+		}
+	} else {
+		return FALSE;
+	}
+	return $participantInfo;
+  }
+	
+/** 
+ ** Helper function to get formatted display names of the the participants 
+ ** Purpose - to generate comma separated display names of primary and additional participants
+**/
+  static function getDisplayNames ( $participantId, $display_name ) {
+	$displayName[] = $display_name;
+	//Get additional participant names
+	$additionalPIds = CRM_Event_BAO_Participant::getAdditionalParticipantIds( $participantId );
+	if ( $additionalPIds ) {
+		foreach ( $additionalPIds as $pid ) {
+			$cId           = CRM_Core_DAO::getFieldValue('CRM_Event_DAO_Participant', $pid, 'contact_id', 'id');
+			$displayName[] = CRM_Contact_BAO_Contact::displayName( $cId );
+		}		
+	} 
+	$displayNames = implode( ', ', $displayName );
+	return $displayNames;
+  }
+ 
+/** 
+ ** Helper function to get related contacts of tthe contact 
+ ** Checks for Child, Spouse, Child/Ward relationship types
+**/
+  static function getRelatedContacts( $contactID ) {
+	//Get relationship type id of Spouse, Child, Child/Ward of 
+	$relTypeIDs             = array();
+	$relTypeIDs['parent']   = CRM_Core_DAO::getFieldValue('CRM_Contact_DAO_RelationshipType', 'Parent of', 'id', 'label_a_b');
+	$relTypeIDs['guardian'] = CRM_Core_DAO::getFieldValue('CRM_Contact_DAO_RelationshipType', 'Parent/Guardian of', 'id', 'label_a_b');
+	$relTypeIDs['spouse']   = CRM_Core_DAO::getFieldValue('CRM_Contact_DAO_RelationshipType', 'Spouse of', 'id', 'label_a_b');
+	$relatedContacts        = CRM_Contact_BAO_Relationship::getRelationship( $contactID );
+	$isRelatedContact       = FALSE;
+	
+	foreach ( $relatedContacts as $relData ) {			
+		if ( array_search ( $relData['civicrm_relationship_type_id'], $relTypeIDs ) ) {
+			$relatedContactIDs[] = $relData['cid'];
+			$isRelatedContact = TRUE;
+		} 
+	}
+	
+	if ( $isRelatedContact ) {
+		return $relatedContactIDs;
+	} else {
+		return FALSE;
+	}
+  }
+
+/** 
+ ** Creates a financial trxn record for the CC transaction of the total amount
+**/    
+  function createFinancialTrxn( $payment ) {
+	//Set Payment processor to Auth CC
+	//To be changed for switching to live processor
+	$payment_processor_id = CRM_Core_DAO::getFieldValue( 'CRM_Financial_DAO_PaymentProcessor', 'Credit Card', 'id', 'name');
+	$fromAccountID        = CRM_Core_DAO::getFieldValue( 'CRM_Financial_DAO_FinancialAccount', 'Accounts Receivable', 'id', 'name' );
+    $CCAccountID          = CRM_Core_DAO::getFieldValue( 'CRM_Financial_DAO_FinancialAccount', 'Payment Processor Account', 'id', 'name' );
+	$paymentMethods       = CRM_Contribute_PseudoConstant::paymentInstrument();
+	$CC_id                = array_search('Credit Card', $paymentMethods);
+	$params = array(
+					'to_financial_account_id'   => $CCAccountID,
+					'from_financial_account_id' => $fromAccountID,
+					'trxn_date'                 => date('Ymd'),
+					'total_amount'              => $payment['amount'],
+					'fee_amount'                => '',
+					'net_amount'                => '',
+					'currency'                  => $payment['currencyID'],
+					'status_id'                 => 1,	
+					'trxn_id'                   => $payment['trxn_id'],
+					'payment_processor'         => $payment_processor_id,
+					'payment_instrument_id'     => $CC_id,
+				);
+	require_once 'CRM/Core/BAO/FinancialTrxn.php';
+	
+	$trxn = new CRM_Financial_DAO_FinancialTrxn();
+	$trxn->copyValues($params);
+	$fids = array();
+	if (!CRM_Utils_Rule::currencyCode($trxn->currency)) {
+	  $config = CRM_Core_Config::singleton();
+	  $trxn->currency = $config->defaultCurrency;
+	}
+
+	$trxn->save();
+	$entityFinancialTrxnParams =
+	 array(
+		'entity_table'      => "civicrm_financial_trxn",
+		'entity_id'         => $trxn->id,
+		'financial_trxn_id' => $trxn->id,
+		'amount'            => $params['total_amount'],
+		'currency'          => $trxn->currency,
+	);
+	$entityTrxn = new CRM_Financial_DAO_EntityFinancialTrxn();
+	$entityTrxn->copyValues($entityFinancialTrxnParams);
+	$entityTrxn->save();
+  }
+
+}
---
+++ templates/CRM/Paymentui/Form/Paymentui.tpl
@@ -0,0 +1,95 @@
+{* HEADER *}
+<div class="form-item">
+	<fieldset>
+	<legend>{ts}{$displayName}{/ts}</legend> 
+		<table class=" form-layout">		
+			<tr>
+				<td colspan="2">
+					<table class="form-layout">   
+						<thead class="sticky"> 
+							{foreach from=$columnHeaders item=header}							  
+								<th scope="col"><strong>{$header}</strong></th>
+							{/foreach}
+						</thead>
+						{foreach from=$participantInfo item=row}
+							<tr class="{$row.rowClass}">
+								<td class="">{$row.event_name}</td>
+								<td class="">{$row.contact_name}</td>
+								<td class="">{$row.total_amount|crmMoney}</td>
+								<td class="">{$row.paid|crmMoney}</td>
+								<td class="">{$row.balance|crmMoney}</td>				
+
+								<td class="">{$form.payment[$row.pid].html|crmMoney}</td>
+								
+							</tr>
+						{/foreach}
+						{if $contactId}
+						<thead class="sticky">  
+									<td colspan = 5 scope="col"><strong>Total</strong></th>
+									<td class="font-size12pt "><span>$ </span><span name='total' id ='total'>0</span></td>                   
+						</thead>
+						{/if}
+					</table>
+				</td>
+			</tr>	
+		</table>						
+	</fieldset>							
+</div>
+
+{* FIELD EXAMPLE: OPTION 1 (AUTOMATIC LAYOUT) *}
+{include file="CRM/Core/BillingBlock.tpl" context="front-end"}
+{if $form.payment_processor.label}
+  {* PP selection only works with JS enabled, so we hide it initially *}
+  <fieldset class="crm-group payment_options-group" style="display:none;">
+    <legend>{ts}Payment Options{/ts}</legend>
+    <div class="crm-section payment_processor-section">
+      <div class="label">{$form.payment_processor.label}</div>
+      <div class="content">{$form.payment_processor.html}</div>
+      <div class="clear"></div>
+    </div>
+  </fieldset>
+  {/if}
+
+  {if $is_pay_later}
+  <fieldset class="crm-group pay_later-group">
+    <legend>{ts}Payment Options{/ts}</legend>
+    <div class="crm-section pay_later_receipt-section">
+      <div class="label">&nbsp;</div>
+      <div class="content">
+        [x] {$pay_later_text}
+      </div>
+      <div class="clear"></div>
+    </div>
+  </fieldset>
+  {/if}
+
+  <div id="billing-payment-block">
+    {* If we have a payment processor, load it - otherwise it happens via ajax *}
+    {if $ppType}
+      {include file="CRM/Contribute/Form/Contribution/Main.tpl" snippet=4}
+    {/if}
+  </div>
+  {include file="CRM/common/paymentBlock.tpl"}
+
+{* FOOTER *}
+<div class="crm-submit-buttons">
+{include file="CRM/common/formButtons.tpl" location="bottom"}
+</div>
+{literal}
+
+<script type="text/javascript">
+function calculateTotal() {
+	var total = 0.00;
+	cj.each(cj( "input[name^='payment']" ), function() { 
+		var amt = cj(this).val();
+		if ( cj.isNumeric(amt) ) {
+			total = parseFloat(total)+parseFloat(amt);
+		}
+	});
+	total = Math.round(total*100, 2)/100;
+    total.toFixed(2);
+	
+	document.getElementById('total').innerHTML = total;		
+}
+</script>
+{/literal}
